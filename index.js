c04b0e4ebc20f13f351295bdb5d7564c9a2b5546
nfluk <nickylukic@gmail.com>
2022-05-08 08:32:10 +0200

    refactored

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 // codewars
 // Check to see if a string has the same amount of 'x's and 'o's.
 
+// function XO(str) {
+//   const strLower = str.toLowerCase();
+
+//   let numberOfX = 0;
+//   let numberOfO = 0;
+
+//   for (let i = 0; i < strLower.length; i++) {
+//     if (strLower[i] === 'x') {
+//       numberOfX += 1;
+//     }
+
+//     if (strLower[i] === 'o') {
+//       numberOfO += 1;
+//     }
+//   }
+
+//   return numberOfX === numberOfO;
+// }
+
+// refactored
+
 function XO(str) {
-  const strLower = str.toLowerCase();
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
 
-  let numberOfX = 0;
-  let numberOfO = 0;
+  console.log(x);
+  console.log(o);
 
-  for (let i = 0; i < strLower.length; i++) {
-    if (strLower[i] === 'x') {
-      numberOfX += 1;
-    }
+  x = x === null ? 0 : x;
+  o = o === null ? 0 : o;
 
-    if (strLower[i] === 'o') {
-      numberOfO += 1;
-    }
-  }
-
-  return numberOfX === numberOfO;
+  return x.length === o.length;
 }
